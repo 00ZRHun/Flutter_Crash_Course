10109c7002288e10a81f83ac9c85af7cdcc76043
00ZRHun <00zrhun@gmail.com>
2023-03-16 20:04:47 +0800

    add baconDescription

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -2,3 +2,6 @@ const double kDouble5 = 5.0;
 const double kDouble10 = 10.0;
 const double kDouble20 = 20.0;
 const double kDouble40 = 40.0;
+
+const String baconDescription =
+    'Bacon ipsum dolor amet turkey biltong pork belly pig, bacon jerky picanha venison beef. Pork loin ribeye doner, spare ribs turducken fatback pork pancetta buffalo. Ball tip bresaola ground round shank boudin picanha leberkas. Beef ribs turducken porchetta sausage alcatra short loin jerky frankfurter andouille. Pancetta shoulder prosciutto pig ham hock ribeye short loin venison corned beef doner.';
