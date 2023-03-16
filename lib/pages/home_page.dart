3bf48623fd3c878cad4dabc5a22960776682876b
00ZRHun <00zrhun@gmail.com>
2023-03-16 12:20:22 +0800

    feat: add Column to add Text & SizedBox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,13 +9,73 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter Mapp'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset("images/rocket.png"),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                // color: Colors.red,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Image.asset("images/rocket.png"),
+                    const Text(
+                      'Title',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text('Description'),
+                    const SizedBox(
+                      height: 10,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset("images/space.png"),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset("images/travel.png"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                // color: Colors.red,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    Image.asset('images/yeah.png'),
+                    const Text(
+                      'title',
+                    ),
+                    const Text('description'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
