09ff7908fab734fe93eeafa5bc1132ce2d84ca06
00ZRHun <00zrhun@gmail.com>
2023-03-16 17:06:25 +0800

    custom widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_youtube/core/constant.dart'; // OPT: '../core/constant.dart'
+import 'package:flutter_youtube/widget/card_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,64 +13,18 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Card(
-              child: Container(
-                padding: const EdgeInsets.all(kDouble10),
-                // color: Colors.red,
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    const SizedBox(height: kDouble5),
-                    Image.asset("images/rocket.png"),
-                    const Text(
-                      'Title',
-                      style: TextStyle(
-                        fontSize: kDouble20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Text('Description'),
-                    const SizedBox(height: kDouble10)
-                  ],
-                ),
-              ),
-            ),
+            const CardWidget(),
             Row(
-              children: [
+              children: const [
                 Expanded(
-                  child: Card(
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset("images/space.png"),
-                    ),
-                  ),
+                  child: CardWidget(),
                 ),
                 Expanded(
-                  child: Card(
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset("images/travel.png"),
-                    ),
-                  ),
+                  child: CardWidget(),
                 ),
               ],
             ),
-            Card(
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                // color: Colors.red,
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    Image.asset('images/yeah.png'),
-                    const Text(
-                      'title',
-                    ),
-                    const Text('description'),
-                  ],
-                ),
-              ),
-            ),
+            const CardWidget(),
           ],
         ),
       ),
