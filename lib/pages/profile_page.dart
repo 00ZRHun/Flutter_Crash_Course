c114c88e4355158e05bbfe66d19f4495e6a884a8
00ZRHun <00zrhun@gmail.com>
2023-03-17 10:45:23 +0800

    complete profile page (add CircleAvatar, ListTile & FloatingActionButton)

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/core/constant.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: Column(
+        children: const [
+          CircleAvatar(
+            backgroundImage: AssetImage('images/yeah.png'),
+            radius: 60,
+          ), // OPT: child: Image.asset('images/yeah.png')
+          SizedBox(
+            height: kDouble20,
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Flutter Mapp"),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("info@fluttermapp.com"),
+          ),
+          ListTile(
+            leading: Icon(Icons.web),
+            title: Text("FlutterMapp.com"),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.dark_mode),
+      ),
+    );
   }
 }
